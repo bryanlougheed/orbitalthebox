1a43658b41d9853746a38cc0999d00d724708eaf
bryanlougheed <bryanlougheed@hotmail.com>
2025-02-21 23:52:23 +0100

    more python packaging bollix

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='orbitalthebox',
-    version='0.1.0',
+    version='0.1.1',
     description='Calculating irradiance for Earth for various astronomical parameters.',
     author='Bryan C. Lougheed',
     author_email='bryan.lougheed@outlook.com',
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         # List your package dependencies here
         'numpy',
